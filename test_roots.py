4a81504d30ecd04094e595ab893d49f8c061765d
Zeyuan Hu <zeyuan_hu@fas.harvard.edu>
2020-10-27 13:31:32 +0800

    quick test by commenting out test for linear root

--- test_roots.py
+++ test_roots.py
@@ -12,13 +12,13 @@ def test_quadroots_zerocoeff():
     with pytest.raises(ValueError):
         roots.quad_roots(a=0.0)
 
-def test_linearoots_result():
-    assert roots.linear_roots(2.0, -3.0) == 1.5
+#def test_linearoots_result():
+#    assert roots.linear_roots(2.0, -3.0) == 1.5
 
-def test_linearroots_types():
-    with pytest.raises(TypeError):
-        roots.linear_roots("ocean", 6.0)
+#def test_linearroots_types():
+#    with pytest.raises(TypeError):
+#        roots.linear_roots("ocean", 6.0)
 
-def test_linearroots_zerocoeff():
-    with pytest.raises(ValueError):
-        roots.linear_roots(a=0.0)
+#def test_linearroots_zerocoeff():
+#    with pytest.raises(ValueError):
+#        roots.linear_roots(a=0.0)
